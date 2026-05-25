f839d08e698d16373c0088e80983adeb9da3494e
datlee27 <datlvde180983@fpt.edu.vn>
2026-05-25 14:06:02 +0700

    setup github pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-})
+  base:
+    mode === 'production'
+      ? '/swp391-su26-ai-audit-project-swp391_se20a02_group-06/'
+      : '/',
+}))
