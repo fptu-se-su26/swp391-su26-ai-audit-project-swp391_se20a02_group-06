78c699bd95b13b0fc63b63536f0aea36635a6caf
datlee27 <datlvde180983@fpt.edu.vn>
2026-05-25 13:59:49 +0700

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,12 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
-import Landing from './pages/public/Landing'
-import Login from './pages/auth/Login'
-import Register from './pages/auth/Register'
-import Pricing from './pages/public/Pricing'
-import About from './pages/public/About'
+import RouterContainer from './router/router-container'
+import { ChakraProvider } from '@chakra-ui/react'
+import theme from './lib/chakra-theme.ts'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        {/* Public Homepage / Landing Page */}
-        <Route path="/" element={<Landing />} />
-        
-        {/* Auth Pages */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        
-        {/* Public Marketing Pages */}
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/about" element={<About />} />
-        
-        {/* Default Route Redirect to Landing */}
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </Router>
+    <ChakraProvider theme={theme}>
+      <RouterContainer />
+    </ChakraProvider>
   )
 }
 
